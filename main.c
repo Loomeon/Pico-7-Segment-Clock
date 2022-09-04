80d87e1e257811ba9e1349fff1b5cd133f21e1a2
Paul Weber <dev.paulweber@posteo.de>
2022-09-04 08:49:54 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+//
+// Created by Paul Weber on 04.09.22.
+//
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main(){
+    setup_default_uart();
+    printf("Hello, world!\n");
+    return 0;
+}
