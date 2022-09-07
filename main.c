8321ebbabe435af48568492200314381824620b3
Paul Weber <dev.paulweber@posteo.de>
2022-09-07 18:50:02 +0200

    Code is now able to display Minutes and Seconds

--- main.c
+++ main.c
@@ -2,34 +2,84 @@
 // Created by Paul Weber on 04.09.22.
 //
 
-#include <stdio.h>
 #include "hardware/rtc.h"
 #include "pico/stdlib.h"
 #include "pico/util/datetime.h"
 #include "hardware/gpio.h"
 
 int main(void);
-void display(int digit, int number, int FIRST_GPIO);
-void display_time(datetime_t *time);
-
-
-
-int Button = 0; //Button to set the Time (which is also used as a interrupt)
-
+//void display(int digit, int number, int *digit_bits, int *number_bits, int digit_mask, int number_mask, int FIRST_GPIO);
+//void display_time(datetime_t *time, int FIRST_GPIO);
 
 int main(void){
 
-    // Start on Friday 5th of June 2020 15:45:00
+
+    int FIRST_GPIO = 0;
+
     datetime_t time = {
             .year  = 2020,
             .month = 06,
             .day   = 05,
             .dotw  = 5, // 0 is Sunday, so 5 is Friday
             .hour  = 15,
-            .min   = 45,
+            .min   = 37,
             .sec   = 00
     };
 
+    //Create Bitmask Arrays-------------------------
+
+    int number_bits[10] = {
+            0x3f,
+            0x06,
+            0x5b,
+            0x4f,
+            0x66,
+            0x6d,
+            0x7d,
+            0x07,
+            0x7f,
+            0x67
+    };
+
+    int digit_bits[4] = {
+            0xE,  // 0
+            0xD,  // 1
+            0xB,  // 2
+            0x7,  // 3
+    };
+
+    int digit_mask = 0xF;
+
+    int number_mask = 0xFF;
+
+    //Convert relative Bitmasks into absolute---------------------------------------
+
+    for(int i=0; i<10; i++){
+        number_bits[i] = number_bits[i] << FIRST_GPIO;
+    }
+
+    for(int i=0; i<4; i++){
+        digit_bits[i] = digit_bits[i] << FIRST_GPIO+8;
+    }
+
+
+    digit_mask = digit_mask << FIRST_GPIO+8;
+
+    number_mask = number_mask << FIRST_GPIO;
+
+
+    //Set GPIO Mode of pins to OUTPUT -----------------------------------------
+
+    // We could use gpio_set_dir_out_masked() here
+    for (int gpio = 0; gpio < 8; gpio++) {
+        gpio_init(gpio);
+        gpio_set_dir(gpio, GPIO_OUT);
+    }
+
+    for (int gpio = 0; gpio < 4; gpio++) {
+        gpio_init(gpio+8);
+        gpio_set_dir(gpio+8, GPIO_OUT);
+    }
 
     //Configuring RTC ----------------------------------------------------------------
 
@@ -37,17 +87,39 @@ int main(void){
     rtc_init();
     rtc_set_datetime(&time);
 
+    int digit[4]; //create an array for all the numbers
+
+
     while(1){
-        display_time(&time);
+
+        //sleep_us(64);
+
+        rtc_get_datetime(&time);
+
+        digit[0] = (time.min/10)%10;
+        digit[1] = (time.min/1)%10;
+
+        digit[2] = (time.sec/10)%10;
+        digit[3] = (time.sec/1)%10;
+
+        //Go through the 4 Digits and Display them
+
+        for(int i=0; i<4; i++){
+
+            gpio_put_masked(number_mask, number_bits[digit[i]]); //Put the bit mask on GPIO
+            gpio_put_masked(digit_mask, digit_bits[i]); //Put the bit mask on GPIO
+
+            sleep_ms(1);
+        }
     }
 
     return 0;
 }
 
 
-
-void display(int digit, int number, int FIRST_GPIO){
 /*
+void display(int digit, int number, int *digit_bits, int *number_bits, int digit_mask, int number_mask, int FIRST_GPIO){
+
 
     Structure
 
@@ -70,67 +142,9 @@ void display(int digit, int number, int FIRST_GPIO){
 
     Pins fit Perfectly into a single Byte, i created a Bitmask for all the Numbers
 
-    */
+
 
 // This array converts a number 0-9 to a bit pattern to send to the GPIOs
-    int number_bits[10] = {
-            0x3f,  // 0
-            0x06,  // 1
-            0x5b,  // 2
-            0x4f,  // 3
-            0x66,  // 4
-            0x6d,  // 5
-            0x7d,  // 6
-            0x07,  // 7
-            0x7f,  // 8
-            0x67   // 9
-    };
 
-    int digit_bits[10] = {
-            0xE,  // 0
-            0xD,  // 1
-            0xB,  // 2
-            0x7,  // 3
-    };
-
-    int number_mask = 0xFF << FIRST_GPIO; //get the right position for the pins
-
-    gpio_put_masked(number_mask, number_bits[number]); //Put the bit mask on GPIO
-
-    int digit_mask = 0xF << FIRST_GPIO+8;
-
-    gpio_put_masked(digit_mask, digit_bits[digit]); //Put the bit mask on GPIO
 }
-
-void display_time(datetime_t *time){
-
-    int digit[4]; //create an array for all the numbers
-
-    rtc_get_datetime(time);
-
-    //Calculate the Digits with modulo
-    digit[1]= time->hour%10;
-    // If the number is smaller than 1, only 1 Digit exists, the other one will be set to 0
-    if(time->hour > 9){
-        digit[0] = (time->hour - digit[1])/10;
-    }
-    else{
-        digit[0] = 0;
-    }
-
-    //Calculate the Digits with modulo
-    digit[3]= time->min%10;
-    if(time->min > 9){
-        // If the number is smaller than 1, only 1 Digit exists, the other one will be set to 0
-        digit[2] = (time->min - digit[1])/10;
-    }
-    else{
-        digit[2] = 0;
-    }
-
-    //Go through the 4 Digits and Display them
-    for(int i; i<4; i++){
-        display(i, digit[i], 1);
-        sleep_ms(25);
-    }
-}
+*/
